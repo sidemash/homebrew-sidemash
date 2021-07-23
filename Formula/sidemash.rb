efec67730f744bfdd32305b41944f064226a8346
Amadou <amadou.beidari@sidemash.com>
2021-07-23 11:16:59 +0200

    pattern shasum

--- Formula/sidemash.rb
+++ Formula/sidemash.rb
@@ -1,8 +1,8 @@
 class Sidemash < Formula
   desc "Command Line Interface to use Sidemash Cloud"
   homepage "https://github.com/sidemash/homebrew-sidemash"
-  url "https://s3.eu-west-1.amazonaws.com/public.sidemash.io/code/sdk-repo-keys/sidemash-cli-brew/sidemash-cli-mac.tar.gz"
-  sha256 "e652d7bf37c1c54017283109f0f43dfa9c972989648cd729c5dcfeecb5b22449"
+  url "https://s3.eu-west-1.amazonaws.com/public.sidemash.io/code/sdk-repo-keys/sidemash-cli-brew/sidemash-mac.tar.gz"
+  sha256 "SHANEWVALUE"
   version "0.1.0"
 
   def install
