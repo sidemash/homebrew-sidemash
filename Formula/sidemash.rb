a8c7c0e78f80f3acc1121a157bad3fbb352fd4fb
sidemashcloud <opensource@sidemash.com>
2021-07-26 15:22:47 +0000

    new release, commit automatically generated

--- Formula/sidemash.rb
+++ Formula/sidemash.rb
@@ -2,7 +2,7 @@ class Sidemash < Formula
   desc "Command Line Interface to use Sidemash Cloud"
   homepage "https://github.com/sidemash/homebrew-sidemash"
   url "https://s3.eu-west-1.amazonaws.com/public.sidemash.io/code/sdk-repo-keys/sidemash-cli-brew/sidemash-mac.tar.gz"
-  sha256 "88c0cd6f39630a58f9ddd16267a41d5a39eb5f1566e1d92e31dade97b8a9c528"
+  sha256 "52659934dbf62f8f87f6ea3fd009fbf1e4d3d8859eeffb8e3e20cde8d7f39642"
   version "0.1.0"
 
   def install
