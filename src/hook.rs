c378e763a2b3d122e4f34c3e42802e70c47b7c38
Amadou <amadou.beidari@sidemash.com>
2021-07-29 10:35:23 +0200

    separate homebrew project

--- src/hook.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Hook {
-    pub (crate) _type: String,
-    pub (crate) method: String,
-    pub (crate) url: String
-}
--- src/stream_square_hook.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-use crate::hook::Hook;
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct StreamSquareHook {
-    pub (crate) ask_stream_start: Hook,
-    pub (crate) tell_stream_started: Hook,
-}
